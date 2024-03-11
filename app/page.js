e4f36bedf256dce614db65b1c84c661849324318
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-03-12 00:32:39 +0530

    Add NextUI Button component and update tailwind.config.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,10 @@
+import { Button } from "@nextui-org/react";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Button />
+    </main>
   );
 }
