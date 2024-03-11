7c5be31d782fa5595138c5f1e14df8fe05eccbce
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-03-12 00:46:01 +0530

    Update function to handle edge cases

---
+++ helper/sendmail.js
@@ -0,0 +1,39 @@
+import nodemailer from "nodemailer";
+
+export async function sendMail(
+  username = "",
+  pass = "",
+  to = "",
+  subject = "Kodessphere OTP",
+  html = "",
+  text = ""
+) {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.zoho.in",
+    secure: true,
+    port: 465,
+    auth: {
+      user: username,
+      pass: pass,
+    },
+  });
+
+  try {
+    await transporter.sendMail({
+      from: '"Konnexions IT Society" <noreply@konnexions.dev>', // sender address
+      to,
+      subject,
+      text,
+      html,
+    });
+    return {
+      success: true,
+      message: "Success",
+    };
+  } catch (error) {
+    return {
+      success: false,
+      message: "Failure",
+    };
+  }
+}
---
+++ prisma/team.js
@@ -0,0 +1,16 @@
+export const SendOTP = async () => {
+  //   try {
+  //     const count = await prisma.team.count();
+  //     return {
+  //       success: true,
+  //       message: "Team count found",
+  //       count: count,
+  //     };
+  //   } catch (error) {
+  //     return {
+  //       success: false,
+  //       message: error.message,
+  //       count: 0,
+  //     };
+  //   }
+};
