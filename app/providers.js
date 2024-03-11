d0aee09a60e98206b06262c117a79b742757d476
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-03-12 00:31:42 +0530

    Add Next UI and Framer Motion dependencies, update layout and globals.css

---
+++ app/providers.js
@@ -0,0 +1,13 @@
+// app/providers.tsx
+"use client";
+
+import { NextUIProvider } from "@nextui-org/react";
+import { SessionProvider } from "next-auth/react";
+
+export function Providers({ children }) {
+  return (
+    <SessionProvider>
+      <NextUIProvider>{children}</NextUIProvider>
+    </SessionProvider>
+  );
+}
