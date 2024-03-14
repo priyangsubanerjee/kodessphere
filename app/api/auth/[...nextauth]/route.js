216919f40046d7889be028fcdd17772a06985ab3
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-03-15 02:38:54 +0530

    Remove unused import statement

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,7 +1,6 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { AuthenticateTeam } from "@/prisma/team";
 import prisma from "@/prisma/prisma";
 
 export const authOptions = {
