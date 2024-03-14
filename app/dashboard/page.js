6b7a73893e853f5a49f4d9232a76bbc20a0bc37a
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-03-15 02:42:52 +0530

    Add sign out button to dashboard page

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,7 +1,15 @@
+"use client";
+import { Button } from "@nextui-org/react";
+import { signOut } from "next-auth/react";
 import React from "react";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  return (
+    <div>
+      Dashboard
+      <Button onClick={() => signOut()}>Sign Out</Button>
+    </div>
+  );
 }
 
 export default Dashboard;
---
+++ middleware.js
@@ -0,0 +1,59 @@
+import { getToken } from "next-auth/jwt";
+import { NextResponse } from "next/server";
+
+export async function middleware(req) {
+  const session = await getToken({
+    req: req,
+    secret: process.env.NEXTAUTH_SECRET,
+  });
+
+  if (req.nextUrl.pathname === "/") {
+    if (session) {
+      return NextResponse.redirect(new URL("/dashboard", req.url));
+    } else {
+      return NextResponse.next();
+    }
+  } else {
+    if (session) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  }
+  //   if (
+  //     req.nextUrl.pathname === "/login" ||
+  //     req.nextUrl.pathname === "/login/otplogin" ||
+  //     req.nextUrl.pathname === "/register" ||
+  //     req.nextUrl.pathname === "/"
+  //   ) {
+  //     if (session) {
+  //       return NextResponse.redirect(new URL("/dashboard", req.url));
+  //     } else {
+  //       if (session?.needPassReset) {
+  //         return NextResponse.redirect(new URL("/password/reset", req.url));
+  //       } else {
+  //         return NextResponse.next();
+  //       }
+  //     }
+  //   } else if (req.nextUrl.pathname === "/password/reset") {
+  //     if (session) {
+  //       return NextResponse.next();
+  //     } else {
+  //       return NextResponse.redirect(new URL("/login", req.url));
+  //     }
+  //   } else {
+  //     if (session) {
+  //       if (session?.needPassReset) {
+  //         return NextResponse.redirect(new URL("/password/reset", req.url));
+  //       } else {
+  //         return NextResponse.next();
+  //       }
+  //     } else {
+  //       return NextResponse.redirect(new URL("/", req.url));
+  //     }
+  //   }
+}
+
+export const config = {
+  matcher: ["/", "/login", "/dashboard"],
+};
