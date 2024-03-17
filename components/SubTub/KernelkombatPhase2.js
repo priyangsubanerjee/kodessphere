fe21df53520cdc74d33aa30fb254d51ccaa2db86
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-03-17 08:21:05 +0530

    Add PS-2 and PS-3 specifics

--- components/SubTub/KernelkombatPhase2.js
+++ components/SubTub/KernelkombatPhase2.js
@@ -14,17 +14,23 @@ import toast from "react-hot-toast";
 function KernelkombatPhase2() {
   const { count, setCount } = useContext(GlobalState);
   const session = useSession();
-  const [colabLink, setColabLink] = React.useState("");
+  const [ps1ColabLink, setPs1ColabLink] = React.useState("");
   const [ps1F1Score, setPs1F1Score] = React.useState("");
   const [ps1Accuracy, setPs1Accuracy] = React.useState("");
   const [ps1WeightFileLink, setPs1WeightFileLink] = React.useState("");
   const [ps1PythonCode, setPs1PythonCode] = React.useState("");
+  const [ps2ColabLink, setPs2ColabLink] = React.useState("");
   const [ps2F1Score, setPs2F1Score] = React.useState("");
   const [ps2Accuracy, setPs2Accuracy] = React.useState("");
   const [ps2Precision, setPs2Precision] = React.useState("");
   const [ps2Recall, setPs2Recall] = React.useState("");
   const [ps2WeightFileLink, setPs2WeightFileLink] = React.useState("");
   const [ps2PythonCode, setPs2PythonCode] = React.useState("");
+  const [ps3ColabLink, setPs3ColabLink] = React.useState("");
+  const [ps3F1Score, setPs3F1Score] = React.useState("");
+  const [ps3Accuracy, setPs3Accuracy] = React.useState("");
+  const [ps3WeightFileLink, setPs3WeightFileLink] = React.useState("");
+  const [ps3PythonCode, setPs3PythonCode] = React.useState("");
 
   const handleSubmit = async () => {
     toast.loading("Submitting...");
@@ -34,17 +40,23 @@ function KernelkombatPhase2() {
       arena: session.data.user.arena,
       gmail: session.data.user.email,
       content: {
-        colabLink: colabLink,
+        ps1ColabLink: ps1ColabLink,
         ps1F1Score: ps1F1Score,
         ps1Accuracy: ps1Accuracy,
         ps1WeightFileLink: ps1WeightFileLink,
         ps1PythonCode: ps1PythonCode,
+        ps2ColabLink: ps2ColabLink,
         ps2F1Score: ps2F1Score,
         ps2Accuracy: ps2Accuracy,
         ps2Precision: ps2Precision,
         ps2Recall: ps2Recall,
         ps2WeightFileLink: ps2WeightFileLink,
         ps2PythonCode: ps2PythonCode,
+        ps3ColabLink: ps3ColabLink,
+        ps3F1Score: ps3F1Score,
+        ps3Accuracy: ps3Accuracy,
+        ps3WeightFileLink: ps3WeightFileLink,
+        ps3PythonCode: ps3PythonCode,
       },
     });
 
@@ -62,27 +74,14 @@ function KernelkombatPhase2() {
     <div className="mt-12 max-w-3xl pb-20">
       <div>
         <div className="font-normal leading-8">
-          <span className="font-semibold">Question 1:</span> Google colab
-          repository link
-        </div>
-        <Textarea
-          placeholder="Paste your Google colab repository link here"
-          className="w-full col-span-2 mt-4"
-          value={colabLink}
-          onChange={(e) => setColabLink(e.target.value)}
-          rows={10}
-          classNames={{
-            input: "pl-3 pt-2",
-            label: "pl-3 pt-4",
-          }}
-        />
-      </div>
-
-      <div className="mt-12">
-        <div className="font-normal leading-8">
-          <span className="font-semibold">Question 2:</span> Specifics for PS-1
+          <span className="font-semibold">Question 1:</span> Specifics for PS-1
         </div>
         <div className="grid grid-cols-2 gap-4 mt-4">
+          <Input
+            label="Colab notebook link"
+            value={ps1ColabLink}
+            onChange={(e) => setPs1ColabLink(e.target.value)}
+          />
           <Input
             label="F1 Score"
             value={ps1F1Score}
@@ -114,9 +113,14 @@ function KernelkombatPhase2() {
 
       <div className="mt-12">
         <div className="font-normal leading-8">
-          <span className="font-semibold">Question 3:</span> Specifics for PS-2
+          <span className="font-semibold">Question 2:</span> Specifics for PS-2
         </div>
         <div className="grid grid-cols-2 gap-4 mt-4">
+          <Input
+            label="Colab notebook link"
+            value={ps2ColabLink}
+            onChange={(e) => setPs2ColabLink(e.target.value)}
+          />
           <Input
             label="F1 Score"
             value={ps2F1Score}
@@ -156,6 +160,47 @@ function KernelkombatPhase2() {
           }}
         />
       </div>
+
+      <div className="mt-12">
+        <div className="font-normal leading-8">
+          <span className="font-semibold">Question 3:</span> Specifics for PS-3
+        </div>
+        <div className="grid grid-cols-2 gap-4 mt-4">
+          <Input
+            label="Colab notebook link"
+            value={ps3ColabLink}
+            onChange={(e) => setPs3ColabLink(e.target.value)}
+          />
+          <Input
+            label="F1 Score"
+            value={ps3F1Score}
+            onChange={(e) => setPs3F1Score(e.target.value)}
+          />
+          <Input
+            label="Accuracy"
+            value={ps3Accuracy}
+            onChange={(e) => setPs3Accuracy(e.target.value)}
+          />
+
+          <Input
+            label="Weight file link (Google Drive)"
+            value={ps3WeightFileLink}
+            onChange={(e) => setPs3WeightFileLink(e.target.value)}
+          />
+        </div>
+        <Textarea
+          placeholder="Python code snippet"
+          className="w-full col-span-2 mt-4"
+          value={ps3PythonCode}
+          onChange={(e) => setPs3PythonCode(e.target.value)}
+          rows={10}
+          classNames={{
+            input: "pl-3 pt-2",
+            label: "pl-3 pt-4",
+          }}
+        />
+      </div>
+
       <div className="flex justify-between mt-16 text-sm">
         <p>
           <span className="font-semibold">Note:</span> You can submit only once.
