54beabbc35d2df6bc0dd8a7d6075a6379192925b
Priyangsu Banerjee <priyangsu26@gmail.com>
2024-03-17 08:59:31 +0530

    Add rules and regulations for the event

--- components/Fragments/Rules.js
+++ components/Fragments/Rules.js
@@ -3,7 +3,7 @@ import React from "react";
 function Rules() {
   return (
     <div className="px-10 py-6">
-      <div className="mt-7">
+      <div className="mt-7 border-b pb-6">
         <h1 className="text-2xl font-semibold">
           Rules & regulations <span className="font-light">for the event</span>
         </h1>
@@ -12,6 +12,43 @@ function Rules() {
           prohibited. Please read the rules carefully before proceeding.
         </p>
       </div>
+      <ul className="list-disc pl-4 mt-10 text-sm space-y-4 leading-8 max-w-4xl">
+        <li>Login using your team id</li>
+        <li>
+          After successfully entering their session details, participants will
+          be directed to the dashboard.
+        </li>
+        <li>
+          Upon reaching the dashboard, participants will encounter the problem
+          statement, based on their arena.
+        </li>
+        <li>
+          Konnexweb & Kognizance participants are required to regularly push
+          their work to their individual GitHub repositories at an interval of 2
+          hours.
+        </li>
+        <li>
+          Participants will have access to their work within the dashboard until
+          12 PM, marking the conclusion of Phase 1.
+        </li>
+        <li>
+          Phase 1 can be submited only once. No further submissions will be
+          accepted after the deadline.
+        </li>
+        <li>Participants will have a break from 2:00 PM to 3:00 PM.</li>
+        <li>
+          Participants are informed that Phase 2 will begin promptly at 4:00 PM
+          and conclude at 6:00 PM.
+        </li>
+        <li>
+          Please note: Attendance is mandatory for all participants in both
+          Phase 1 (until 12 PM) and Phase 2 (from 4 PM to 6 PM).
+        </li>
+        <li>
+          Discourage and condemn API abuse practices such as excessive requests,
+          data scraping beyond allowed limits, or unauthorized access attempts.
+        </li>
+      </ul>
     </div>
   );
 }
